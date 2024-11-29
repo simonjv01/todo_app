40523936e035c071c49e77f6caa9e45300c385f6
Simon Vargas <sjvargas01@gmail.com>
2024-11-29 08:49:34 -0500

    refactored for loop

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ todos = []
 
 while True:
     user_action = input("Type add, show, or exit:")
+    user_action = user_action.strip()
 
     match user_action:
         case "add":
             todo = input("Enter a todo item:")
             todos.append(todo)
         case "show":
-            for todo in todos:
-                print(todo)
+            for item in todos:
+                print(item)
         case "exit":
             break
-        case _:
+        case whatever:
             print("Invalid action. Please try again.")
+
+print("Bye!")
 
 
 
