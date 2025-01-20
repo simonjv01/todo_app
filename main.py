7be1c0de5d42307d2c1e936807d9bc5e6e68d8d0
Simon Vargas <sjvargas01@gmail.com>
2025-01-20 18:39:26 -0500

    organize code

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
+from functions import get_todos, write_todos
 
-
-def get_todos(filepath="files/todos.txt"):
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="files/todos.txt"):
-     with open(filepath, "w") as file_local:
-        file_local.writelines(todos_arg)
 
 while True:
     user_action = input("Type add or new then a todo, show, complete, edit, or exit:")
