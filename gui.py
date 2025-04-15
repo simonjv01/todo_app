91671445cca801042f4a7aaea98403d15dbcb1f5
Simon Vargas <sjvargas01@gmail.com>
2025-04-14 21:10:58 -0400

    updated window

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ label = sg.Text("Type in a todo")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("My Todo App", layout=[[label], [input_box], [add_button]])
+window = sg.Window("My Todo App", layout=[[label], [input_box], [add_button]], font=("Helvetica", 20))
 
 window.read()
 
