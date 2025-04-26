05135a8f2b957837c85caac40410bace83df5496
Simon Vargas <sjvargas01@gmail.com>
2025-04-25 21:14:17 -0400

    adding function to edit button

--- gui.py
+++ gui.py
@@ -18,13 +18,21 @@ window = sg.Window("My Todo App",
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
+            functions.write_todos(todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + "\n"
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
             functions.write_todos(todos)
         case sg.WIN_CLOSED:
             break       
